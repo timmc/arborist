8ea545c93bebf5281a0eb43bf655ece45b5207bb
Tim McCormack <cortex@brainonfire.net>
2013-01-19 14:46:34 -0500

    Handle seqs and lists.

--- src/org/timmc/arborist.clj
+++ src/org/timmc/arborist.clj
@@ -9,7 +9,9 @@ Tips:
   the dispatch function should return a transformer that calls the
   walk function manually. Remember to preserve metadata when implementing.
 * Arborist does not normally descend into metadata, but a transformer could
-  again do this manually.")
+  again do this manually.
+* Seq-walking is lazy. To force seqs, add a dispatch clause such as
+  `(seq? %) (comp doall (partial walk dispatch))")
 
 (defn walk
   "Walk a data structure recursively. The dispatch function
@@ -21,11 +23,21 @@ and its results are used to replace the form.
 Descent does not continue into the results of a replacement; it is up to
 the transformer to call the walk function as necesary on child nodes.
 
+Nota bene:
+* Seqs are walked lazily.
+* walk will not descend into sequential? values that do not pass one of
+  list?, seq?, or vector?, since it cannot guarantee the ordering of the
+  output.
+
 Metadata and collection types are preserved."
   [dispatch form]
   (if-let [transform (dispatch form)]
     (transform form)
     (cond
+     ;; unknown type, don't know how to recurse
+     (not (coll? form))
+     form
+
      ;; Maps are handled specially -- transformation of a KV pair
      ;; is really an assoc and/or dissoc.
      (map? form)
@@ -44,10 +56,24 @@ Metadata and collection types are preserved."
                       (rest pairs)))))
          out))
 
-     (coll? form)
-     ;; #'into strips metadata :-/
-     (with-meta (into (empty form) (map (partial walk dispatch) form))
+     ;; Lists can't use #'into, since they'd get reversed.
+     (list? form)
+     (with-meta (apply list (map #(walk dispatch %) form))
        (meta form))
 
-     :else ;; unknown type, don't know how to recurse
+     ;; Seqs are handled lazily
+     (seq? form)
+     (with-meta (map #(walk dispatch %) form)
+       (meta form))
+
+     ;; Vectors can use #'into and #'empty since they conj to the end.
+     ;; So can sets, since they aren't ordered.
+     (or (vector? form)
+         (not (sequential? form)))
+     (with-meta (into (empty form) (map #(walk dispatch %) form))
+       (meta form))
+
+     ;; Can't safely process arbitrary sequential collections, since
+     ;; we don't know if they conj to the end.
+     :else
      form)))
--- test/org/timmc/arborist_test.clj
+++ test/org/timmc/arborist_test.clj
@@ -11,7 +11,7 @@
                :protect 6
                9 :increment
                :sets (with-meta #{:foo 5} {:meta :data})
-               [[:hi]] :is-a-vec}
+               [[:hi] :foo] :is-a-vec}
         protected-entry? #(when (instance? java.util.Map$Entry %)
                             (or (= (key %) :protect)
                                 (= (val %) :protect)))
@@ -25,6 +25,16 @@
                    :protect 6
                    10 :increment
                    :sets #{:foo 6}
-                   [[:bye]] :is-a-vec}))
+                   [[:bye] :foo] :is-a-vec}))
     (is (instance? Rec (:rec output)))
     (is (= (meta (:sets output)) {:meta :data}))))
+
+(deftest guarding
+  (is (= (walk #(when (integer? %) inc) [(take 3 (range)) '(10 11)])
+         '[(1 2 3) (11 12)]))
+  (let [inf (range)
+        in [inf 4]
+        out (walk #(cond (instance? clojure.lang.LazySeq %) identity
+                         (integer? %) inc)
+                  in)]
+    (is (= out [inf 5]))))
